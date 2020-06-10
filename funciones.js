75b371d236911925daf708dc392e9eb1557959a8
McPab <pabmunoz@ucm.es>
2020-06-10 23:02:48 +0200

    serviceworker2

--- funciones.js
+++ funciones.js
@@ -1,6 +1,16 @@
 window.onload=function cargar(){
 
-
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+          // Registration was successful
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+          // registration failed :(
+          console.log('ServiceWorker registration failed: ', err);
+        });
+      });
+    }
     var boton1 = "Tarea 1";
     var boton2 = "Tarea 2";
 
