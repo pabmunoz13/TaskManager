21eae8790f79f4bae91316b1c324149b53bc679a
McPab <pabmunoz@ucm.es>
2020-06-10 22:36:19 +0200

    app con localStorage

--- funciones.js
+++ funciones.js
@@ -1,18 +1,90 @@
-/*
-function guardar(){
-    let boton = {
-        posicion: 4,
-        nombre: "tarea4"
-    }
-    let prueba = "Juan";
-    localStorage.setItem("boton", JSON.stringify(boton));
-}*/
+window.onload=function cargar(){
 
-function cargar(){
-    let boton = JSON.parse( localStorage.getItem("boton"));
-    console.log (boton)
+    var boton1 = "Tarea 1";
+    var boton2 = "Tarea 2";
+
+    localStorage.setItem("boton1", boton1);
+    localStorage.setItem("boton2", boton2);
+    cargarEtiquetas();
 }
 
+function cargarEtiquetas(){
+
+    var keys = Object.keys(localStorage);
+    for (var a in keys){
+
+         console.log(a, ' = ', localStorage.key(a));
+         var nombre =localStorage.getItem(localStorage.key(a));
+         console.log("**"+nombre);
+         var div = document.getElementById("bot");
+
+         var lastChildID = document.getElementById("bot").lastChild.id;
+
+
+         if(lastChildID !== undefined){
+
+             var valor = lastChildID.substr(lastChildID.length-1, lastChildID.length);
+             var id = parseInt(valor)+1;
+
+         }else{
+            var id = 1;
+         }
+         /*BOTON!*/
+         console.log("valor id =======> "+id);
+         var node = document.createElement("input");
+         node.setAttribute("id",id);
+         node.setAttribute("type","button");
+         node.setAttribute("value",nombre);
+         node.setAttribute("name",nombre);
+         node.setAttribute("class","boton");
+         node.setAttribute("onClick","editarTarea('"+id+"')");
+         div.appendChild(node);
+
+         /*DIV CONTENEDOR DE BOTONES*/
+         var node2 = document.createElement("div");
+         node2.setAttribute("id","div"+id);
+         node2.setAttribute("class","contenedor5");
+         div.appendChild(node2);
+
+         /*INPUT TEXT*/
+         var node3 = document.createElement("input");
+         node3.setAttribute("type","text");
+         node3.setAttribute("id","textoTarea"+id);
+         document.getElementById("div"+id).appendChild(node3);
+
+         /*DIV FUNCIONALIDAD*/
+         var node4 = document.createElement("div");
+         node4.setAttribute("class","contenedor6");
+         node4.setAttribute("id","func"+id);
+         document.getElementById("div"+id).appendChild(node4);
+
+         /*BOTON ELIMINAR*/
+         var node5 = document.createElement("input");
+         node5.setAttribute("type","button");
+         node5.setAttribute("value","Eliminar");
+         node5.setAttribute("class","boton_tarea");
+         node5.setAttribute("onClick","eliminarTarea('"+id+"')");
+         document.getElementById("func"+id).appendChild(node5);
+
+         /*BOTON CANCELAR*/
+         var node6 = document.createElement("input");
+         node6.setAttribute("type","button");
+         node6.setAttribute("value","Cancelar");
+         node6.setAttribute("class","boton_tarea");
+         node6.setAttribute("onClick","cancelarTarea('"+id+"')");
+         document.getElementById("func"+id).appendChild(node6);
+
+         /*BOTON GUARDAR*/
+         var node7 = document.createElement("input");
+         node7.setAttribute("type","button");
+         node7.setAttribute("value","Guardar");
+         node7.setAttribute("class","boton_tarea");
+         node7.setAttribute("onClick","guardarTarea('"+id+"')");
+         document.getElementById("func"+id).appendChild(node7);
+
+    }
+
+}
 /*PARTE DE ARRIBA*/
 
 function guardar() {
@@ -22,7 +94,6 @@ function guardar() {
     var lastChildID = document.getElementById("bot").lastElementChild.id;
     var valor = lastChildID.substr(lastChildID.length-1, lastChildID.length);
     var id = parseInt(valor)+1;
-    console.log("PRUEBA ULTIMO ID => "+valor);
 
     /*BOTON!*/
     var node = document.createElement("input");
@@ -76,19 +147,7 @@ function guardar() {
     node7.setAttribute("onClick","guardarTarea('"+id+"')");
     document.getElementById("func"+id).appendChild(node7);
 
- //   nodo2.appendChild(node4);
-
-   // div.appendChild(node2);
-/*    <input type="button" onclick="editarTarea()" class="boton" id="4" type="button" value="Tarea4"></input>
-                <div id="div4" class="contenedor5">
-                    <input type="text" id="textoTarea4"></input>
-                    <div class="contenedor6">
-                        <input type="button" class="boton_tarea"  onclick="eliminarTarea('4')" value="Eliminar"></input>
-                        <input type="button" class="boton_tarea"  onclick="cacelarTarea('4')" value="Cancelar"></input>
-                        <input type="button" class="boton_tarea"  id="pepe" onclick="guardarTarea('4')" value="Guardar"></input>
-                    </div>
-                </div>*/
-
+    localStorage.setItem("boton"+id, nombre.value);
 }
 
 function addTarea() {
@@ -120,7 +179,6 @@ function editarTarea(id){
     x.style.display = "block";
   }
     let boton = document.getElementById(id).value;
-    //localStorage.setItem("botonrr", boton);
 }
 
 function eliminarTarea(id) {
@@ -129,6 +187,7 @@ function eliminarTarea(id) {
   var divHijo = document.getElementById("div"+id);
   divPadre.removeChild(input);
   divPadre.removeChild(divHijo);
+  localStorage.removeItem("boton"+id);
 }
 
 function cancelarTarea(id) {
@@ -141,4 +200,6 @@ function cancelarTarea(id) {
 function guardarTarea(id) {
     var idf = "textoTarea"+id;
     document.getElementById(id).value = document.getElementById(idf).value;
+
+    localStorage.setItem("boton"+id, document.getElementById(idf).value);
 }
