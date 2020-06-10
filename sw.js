3e719b3b38a014cb816c4519cc49efc9100e3879
McPab <pabmunoz@ucm.es>
2020-06-11 00:04:55 +0200

    serviceworker6

--- sw.js
+++ sw.js
@@ -1,10 +1,45 @@
 /* eslint strict: 0 */
 
 'use strict';
+
+
+const CACHE="v1",
+urls=[
+'./funciones.js',
+'./estilo.css'
+]
+
 self.addEventListener('install', event => {
     console.log("evento install "+event);
+    event.waitUntil(
+    caches.open(CACHE).then(cache => {
+    return cache.addAll(urls)
+    .then(()=>self.skipWaiting())})
+    )
+    .catch(err=> console.log('fallo registro caché', err))
+});
+
+self.addEventListener('activate', event => {
+    console.log("evento activate "+event);
+    const cacheWhitelist = [CACHE]
+    event.waitUntil(
+    caches.keys().then(cachesNames => {
+        cachesNames.map(cachesName => {
+            if(cacheWhitelist.indexOf(cachesName) === -1){
+            return caches.delete(cachesName)}
+        })
+    }).then(()=> self.clients.claim())
+    )
 });
 
 self.addEventListener('fetch', event => {
     console.log("evento fetch "+event);
+    event.respondWith(
+        caches.match(event.request).then(res => {
+            if(res){
+                return res
+            }
+            return fetch(event.request)
+        })
+    )
 });
